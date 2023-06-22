13309d5abcd9dc9b354d7d1ff83ea3b11fce7a50
우창민 <rebuilding33@gmail.com>
2023-06-22 21:48:44 +0900

    feat : resources folder수정

--- Assets/Resources/EquipItemData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ItemData", menuName = "EquipItem")]
-public class EquipItemData : ScriptableObject
-{
-    
- }
