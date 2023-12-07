c9221bc600dfbf1731be48862962af143fb76a73
WooChangMin <rebuilding33@gmail.com>
2023-12-07 18:58:50 +0900

    feat : UIScript foldering

--- Assets/Scripts/UIScript/MenuUI_.cs
+++ Assets/Scripts/UIScript/MenuUI_.cs
@@ -7,10 +7,18 @@ public class MenuUI_ : MonoBehaviour
     [SerializeField] private GameObject MenuCanvas;
     [SerializeField] private GameObject SaveCanvas;
 
+    [SerializeField] private GameObject startButton;
+    [SerializeField] private GameObject saveButton_1;
+
     private void Start()
     {
         MenuCanvas.SetActive(true);
         SaveCanvas.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        GameManager.UI.SetFocusUI(startButton);
     }
     public void StartGame()
     {
@@ -21,16 +29,26 @@ public class MenuUI_ : MonoBehaviour
     {
         MenuCanvas.SetActive(false);
         SaveCanvas.SetActive(true);
+        GameManager.UI.SetFocusUI(saveButton_1);
     }
     public void QuitGame()
     {
         Application.Quit();
     }
+    
+    public void Save1()
+    {
 
+    }
+    public void Save2()
+    {
+
+    }
     public void BackMenu()
     {
         MenuCanvas.SetActive(true);
         SaveCanvas.SetActive(false);
 
+        GameManager.UI.SetFocusUI(startButton);
     }
 }
