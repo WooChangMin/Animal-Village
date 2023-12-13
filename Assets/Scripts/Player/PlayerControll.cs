7f3aadba80e7ae8e132926680e5dc71329670a4c
WooChangMin <rebuilding33@gmail.com>
2023-12-13 16:59:24 +0900

    feat : Add particle system Code

--- Assets/Scripts/Player/PlayerControll.cs
+++ Assets/Scripts/Player/PlayerControll.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
-
 public class PlayerControll : MonoBehaviour
 {
     //인벤토리 종료시 발생하는 이벤트
@@ -21,16 +19,15 @@ public class PlayerControll : MonoBehaviour
     //인벤토리용
     [SerializeField] Transform dropPoint;
     [SerializeField] LayerMask itemMask;
-    public InventoryObject inventory;
-    private bool activeInventory = false;
 
-    private ParticleSystem runEffect;
+    public InventoryObject inventory;
+
+    private ParticleSystem curParticle;
 
     //달리고있는지 유무확인
-    private bool isRun;
-
+    private bool isRun = false;
     //인벤토리가 열려있을때 다른행동 정지
-
+    private bool onInventory = false;
     //줍는 중인경우! 다른행동정지
     private bool isPick = false;
 
@@ -38,21 +35,23 @@ public class PlayerControll : MonoBehaviour
     private Vector3 moveDir;
     private CharacterController controller;
 
+    ParticleSystem[] myPartList;
+
 
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
-        runEffect = GetComponentInChildren<ParticleSystem>();
 
+        myPartList = GetComponentsInChildren<ParticleSystem>();
     }
 
     private void Update()
     {
-        if (!isPick || !activeInventory)
+        if (!isPick || !onInventory)
             Move();
 
-        if (activeInventory)
+        if (onInventory)
             InventoryCursor();
     }
 
@@ -90,19 +89,25 @@ public class PlayerControll : MonoBehaviour
     //움직임 구현 (뛸때 걸을떄 속도조절)
     private void Move()
     {
-        if (!activeInventory & !isPick)
+        //Particle System Management
+        ParticleManage();
+
+        if (!onInventory & !isPick)
         {
-            runEffect.Play();
+            //입력값이 없을때
             if (moveDir.magnitude == 0)
             {
                 curSpeed = Mathf.Lerp(curSpeed, 0f, 0.5f);
-                runEffect.Stop();
             }
+            
+            //달릴때
             else if (isRun == true)
             {
                 curSpeed = Mathf.Lerp(curSpeed, runSpeed, 0.03f);
                 transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(-moveDir), Time.deltaTime * rotateSpeed);
             }
+
+            //걸을때
             else
             {
                 curSpeed = Mathf.Lerp(curSpeed, moveSpeed, 0.05f);
@@ -112,13 +117,42 @@ public class PlayerControll : MonoBehaviour
             controller.Move(-moveDir* curSpeed * Time.deltaTime);
             animator.SetFloat("Speed", curSpeed);
         }
-        else
-        {
-
-        }
     }
 
-    // 중력 조절
+    private void ParticleManage()
+    {
+        if(curParticle !=null)
+            curParticle.Stop();
+
+        if(curSpeed > 6)
+        {
+            // **** run Effect ****
+            myPartList[0].gameObject.SetActive(false);
+            myPartList[1].gameObject.SetActive(true);
+            curParticle = myPartList[1];
+        }
+
+        else if(curSpeed > 2)
+        {
+            // **** walk Effect ****
+            myPartList[0].gameObject.SetActive(true);
+            myPartList[1].gameObject.SetActive(false);
+            curParticle = myPartList[0];
+        }
+
+        else
+        {
+            // **** none Effect ****
+            myPartList[0].gameObject.SetActive(false);
+            myPartList[1].gameObject.SetActive(false);
+            curParticle = null;
+        }
+
+        if (curParticle!=null)
+            curParticle.Play();
+    }
+
+    // 중력 조절 *FixedUpdate에 구현
     private void Fall() 
     {
         controller.Move(Vector3.up * Physics.gravity.y * Time.deltaTime);
@@ -146,7 +180,7 @@ public class PlayerControll : MonoBehaviour
     {
         while (true)
         {
-            if (isPick && !activeInventory)
+            if (isPick && !onInventory)
             { 
                 //발위치에서 원형의 오버랩스피어사용                    
                 Collider[] colliders = Physics.OverlapSphere(dropPoint.position, 0.7f, itemMask);
@@ -186,11 +220,11 @@ public class PlayerControll : MonoBehaviour
         ItemPickUp();
     }
 
-    //인벤토리 함수로 activeInventory의 bool값을 바꾸면서 UI매니저에서 구현
+    //인벤토리 함수로 onInventory의 bool값을 바꾸면서 UI매니저에서 구현
     private void Inventory()
     {
-        activeInventory = !activeInventory;
-        if (activeInventory)
+        onInventory = !onInventory;
+        if (onInventory)
         {
             animator.SetBool("IsOnInven", true);
             OnInventoryOpen?.Invoke();
